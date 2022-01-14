3be5a3b0f4fb7231dd734009b3021deb95fd0523
Jose Guzman <Josemguzman784@gmail.com>
2022-01-14 11:11:58 -0600

    hw5

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// global variables
+
+// functions
+// save task in local storage
+function saveTask() {
+    console.log($(this).parent());
+
+    const time = $(this).parent().attr("id");
+    const task = $(this).siblings("textarea").val();
+
+    localStorage.setItem(time, task);
+}
+
+// event listeners
+// show current date
+$("#currentDay").text(moment().format("MMM Do, YYYY"));
+
+// retrieve tasks from local storage
+$("#hour9 .js-task").val(localStorage.getItem("hour9"));
+$("#hour10 .js-task").val(localStorage.getItem("hour10"));
+$("#hour11 .js-task").val(localStorage.getItem("hour11"));
+$("#hour12 .js-task").val(localStorage.getItem("hour12"));
+$("#hour1 .js-task").val(localStorage.getItem("hour1"));
+$("#hour2 .js-task").val(localStorage.getItem("hour2"));
+$("#hour3 .js-task").val(localStorage.getItem("hour3"));
+$("#hour4 .js-task").val(localStorage.getItem("hour4"));
+$("#hour5 .js-task").val(localStorage.getItem("hour5"));
+// user clicks save button
+$(".js-btn").on("click", saveTask);
